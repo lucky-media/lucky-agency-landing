9b52e5a581a4ce0166f5cd7262aa8c5ce12fd50e
lokmanm <lokman.musliu@gmail.com>
2019-10-09 15:57:45 +0200

    Slider fix and implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,33 @@
 import style from "../scss/app.scss";
 import Navigation from "./components/navigation";
+import {
+  tns
+}
+from "tiny-slider/src/tiny-slider.module.js";
+
 
 document.addEventListener('DOMContentLoaded', () => {
   new Navigation();
 });
+
+const slideNext = document.querySelector('.slideNext');
+const slidePrev = document.querySelector('.slidePrev');
+
+const slider = tns({
+  container: '.slides',
+  items: 4,
+  slideBy: "1",
+  speed: 400,
+  autoWidth: true,
+  controls: false,
+  loop: true,
+  edgePadding: 0,
+})
+
+slideNext.addEventListener("click", (event) => {
+  slider.goTo('next');
+})
+
+slidePrev.addEventListener("click", (event) => {
+  slider.goTo('prev');
+})
